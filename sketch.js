8273b61f0e74bbfbcdeb5d82c676967483852736
Aniruddha12709 <85880752+Aniruddha12709@users.noreply.github.com>
2021-06-20 14:14:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,9 +7,33 @@ var b = 0;
 
 function setup() {
   createCanvas(400, 400);  
+  
+  btn_red = createbutton("RED");
+  btn_red.position(100,50);
+  btn_red.mousePressed(red_bg);
+  
+  btn_green = createbutton("GREEN");
+  btn_green.position(250,50,50);
+  btn_green.mousePressed(green_bg);
+  
 }
 
 function draw() {
   background(r,g,b);
 }
+
+function red_bag();
+{
+  r = 255;
+  g = 0;
+  b = 0;
+}
+
+function green_bag()
+{
+  r = 0;
+  g = 255;
+  b = 0;
+}
+
 
